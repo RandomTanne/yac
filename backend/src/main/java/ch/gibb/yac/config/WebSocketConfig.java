207012c8f929f2b7504823666cb8dc493420c4d0
Arthur Deierlein <info@c0rydoras.dev>
2025-05-06 10:44:22 +0200

    feat: /hello endpoint that sends stuff based on get requests

--- backend/src/main/java/ch/gibb/yac/config/WebSocketConfig.java
+++ backend/src/main/java/ch/gibb/yac/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package ch.gibb.yac.config;
 import ch.gibb.yac.handlers.HelloWebSocketHandler;
 import ch.gibb.yac.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,8 +15,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     PersonRepository personRepository;
 
+    @Bean
+    public HelloWebSocketHandler helloWebSocketHandler() {
+        return new HelloWebSocketHandler(personRepository);
+    }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new HelloWebSocketHandler(personRepository), "/hello").setAllowedOrigins("*");
+        registry.addHandler(helloWebSocketHandler(), "/hello").setAllowedOrigins("*");
     }
 }
