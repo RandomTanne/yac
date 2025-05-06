f4fdfa323e9481834f0d1583ddd4c0a0a7bdb5fd
Manuel Möri <manuel07@bluewin.ch>
2025-05-06 08:28:52 +0200

    Add login and signup component without functionality and include tailwind in project

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
+
+export class login {
+}
