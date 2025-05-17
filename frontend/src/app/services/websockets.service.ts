4cb8686989cb70e974fdf1ba039c750705a023b1
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2025-05-17 18:29:53 +0200

    Correctly type websocket subject in frontend

--- frontend/src/app/services/websockets.service.ts
+++ frontend/src/app/services/websockets.service.ts
@@ -1,22 +1,19 @@
 import { Injectable } from "@angular/core";
 import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 import { Observable } from "rxjs";
+import {WebsocketMessage} from '../../types';
 
 @Injectable({
   providedIn: "root",
 })
 export class WebsocketsService {
-  private socket$: WebSocketSubject<any>;
+  private socket$: WebSocketSubject<WebsocketMessage>;
 
   constructor() {
     this.socket$ = webSocket("ws://localhost:8080/sockets/chatrequests");
   }
 
-  sendMessage(message: any) {
-    this.socket$.next(message);
-  }
-
-  getMessages(): Observable<any> {
+  getMessages(): Observable<WebsocketMessage> {
     return this.socket$.asObservable();
   }
 
