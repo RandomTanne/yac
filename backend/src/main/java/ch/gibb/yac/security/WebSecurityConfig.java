28ff6b2b9f0e10f8200619952b53fc18cd7b3c54
Jannik Pulfer <jannik.pulfer@icloud.com>
2025-12-18 10:03:40 +0100

    fix: add production url to allowed origins in backend

--- backend/src/main/java/ch/gibb/yac/security/WebSecurityConfig.java
+++ backend/src/main/java/ch/gibb/yac/security/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost", "http://46.62.203.236"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
