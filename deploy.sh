ed13eaa9f9469f5b53298a771df84dbfb07e29c7
Jannik Pulfer <jannik.pulfer@icloud.com>
2026-01-22 09:11:36 +0100

    feat: move caddy to its own image and push it to ghcr

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@ export DOCKER_HOST=ssh://yac-user@46.62.203.236:2222
 read -p "Enter image tag: " tag
 export BACKEND_TAG="$tag"
 export FRONTEND_TAG="$tag"
+export CADDY_TAG="$tag"
 
 echo "Starting application on ${DOCKER_HOST} with version ${FRONTEND_TAG}"
 
+docker compose -f compose.yaml -f compose.production.yaml down -v
 docker compose -f compose.yaml -f compose.production.yaml up -d
